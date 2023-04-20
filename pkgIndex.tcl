191b872ed682e26987eac4c42a0c33711dfa72ce
Alex Baker <ambaker1@mtu.edu>
2023-04-20 16:03:18 -0500

    Added files
    Simple package intended for Tin unit tests

---
+++ pkgIndex.tcl
@@ -0,0 +1,1 @@
+package ifneeded tintest 0.1 [list source [file join $dir tintest.tcl]]
---
+++ tintest.tcl
@@ -0,0 +1,18 @@
+# A package for Tin unit tests
+
+namespace eval ::tintest {
+    variable message "Hello World!"
+    namespace export foo bar
+}
+
+proc ::tintest::foo {} {
+    variable message
+    return $message
+}
+
+proc ::tintest::bar {} {
+    variable message
+    return [string toupper $message]
+}
+
+package provide tintest 0.1

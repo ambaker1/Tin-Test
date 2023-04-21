f94eee7808be94196d83dde65442d20b39408308
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-20 19:04:20 -0500

    Added option to foo and bar

--- tintest.tcl
+++ tintest.tcl
@@ -1,22 +1,22 @@
 # A package for Tin unit tests
 
 namespace eval ::tintest {
-    variable message "Hello World!"
+    variable greeting "Hello"
     namespace export foo bar foobar
 }
 
-proc ::tintest::foo {} {
-    variable message
-    return $message
+proc ::tintest::foo {{who World}} {
+    variable greeting
+    return "$greeting $who!"
 }
 
-proc ::tintest::bar {} {
-    variable message
-    return [string toupper $message]
+proc ::tintest::bar {{who World}} {
+    variable greeting
+    return [string toupper "$greeting $who!"]
 }
 
 proc ::tintest::foobar {} {
     return [list [foo] [bar]]
 }
 
-package provide tintest 0.2
+package provide tintest 0.3

c57e918ad46ba3fff1abccb7d05005589244792b
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-05-08 22:37:04 -0500

    Update install.tcl

--- install.tcl
+++ install.tcl
@@ -2,4 +2,4 @@ set version 1.2a0
 set dir [file join [file dirname [info library]] "tintest-$version"]
 file delete -force $dir
 file mkdir $dir
-file copy -force pkgIndex.tcl pkgUninstall.tcl tintest.tcl README.md LICENSE $dir
+file copy -force pkgIndex.tcl tintest.tcl README.md LICENSE $dir
